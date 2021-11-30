478ca6f721eb892a37c0ecd5f4040c878e2a34a1
GitHub Action <action@github.com>
2021-11-30 06:36:40 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tang5011235/mvpmarstemplet/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tang5011235.mvpmarstemplet.services
+
+import com.github.tang5011235.mvpmarstemplet.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
